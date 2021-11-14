15ee0e7bed4db57da65f03cb03220d4dce0f783e
GuillermoAVelasco <guillermo.a.velasco@gmail.com>
2021-11-14 17:34:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const server = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
